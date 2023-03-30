a98e061376179a12f86e50e93a8e18f845e41d09
acfielder <afielder@ithaca.edu>
2023-03-30 11:59:24 -0400

    added Product constructor

--- src/main/java/edu/ithaca/barr/bank/Product.java
+++ src/main/java/edu/ithaca/barr/bank/Product.java
@@ -2,4 +2,21 @@ package edu.ithaca.barr.bank;
 
 public class Product {
     
+    private String name;
+    private int inventoryTotal = 10;
+    private int location;
+    private String date;
+    private double price;
+    private int id;
+
+    public Product(String nameIn, int locationIn, String dateIn, double priceIn, int idIn){
+        name = nameIn;
+        location = locationIn;
+        date = dateIn;
+        price = priceIn;
+        id = idIn;
+    }
+
+
+    
 }
