70e29c5c289978c9f2aadf4563261681ec6f6bcf
acfielder <afielder@ithaca.edu>
2023-03-30 12:39:00 -0400

    added productAlertStock method headers and tests

--- src/main/java/edu/ithaca/barr/bank/Manager.java
+++ src/main/java/edu/ithaca/barr/bank/Manager.java
@@ -1,12 +1,26 @@
 package edu.ithaca.barr.bank;
 
 public class Manager extends Employee{
-    private int id;
-    private String name;
     
     public Manager(int idIn, String nameIn){
         super(idIn, nameIn);
         
+    }
+
+
+    /**
+     * @param productId id of product to check stock of
+     * @return string clarifying if product is low on stock or not as well as how many of the product remaining if low
+     */
+    public String alertLowStock(int productId){
+        return null;
+    }
+
+    /**
+     * @return string of all products running low on stock as well as how many of the product remaining if low
+     */
+    public String alertLowStock(){
+        return null;
     }
 
 }
