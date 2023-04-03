19fb6786a8c013f996279c7e80592c8aa3bd1958
mattweil11 <mattweil11@gmail.com>
2023-04-03 10:21:58 -0400

    Fixed Tests and made them more representative of the actual task of alerting the manager based on expiration date

--- src/test/java/edu/ithaca/barr/bank/ProductAlertExpirationTest.java
+++ src/test/java/edu/ithaca/barr/bank/ProductAlertExpirationTest.java
@@ -10,132 +10,84 @@ import static org.junit.jupiter.api.Assertions.*;
  * @date 03/31/2023
  */
 
-import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import java.time.LocalDate;
-import java.util.ArrayList;
-
-public class ProductAlertExpirationTest {
-
-    private Manager manager;
-    private Product product1;
-    private Product product2;
-    private Product product3;
-
-    @BeforeEach
-    void setUp() {
-        manager = new Manager(123, "John Doe");
-        product1 = new Product("Product 1", LocalDate.now().plusDays(5), 10.0, 1);
-        product2 = new Product("Product 2", LocalDate.now().plusDays(10), 20.0, 2);
-        product3 = new Product("Product 3", LocalDate.now().plusDays(15), 30.0, 3);
-    }
-
-    @Test
-    void testAlertProductExpirationWithExistingProductId() {
-        // Arrange
-        ArrayList<Product> products = new ArrayList<>();
-        products.add(product1);
-        products.add(product2);
-        products.add(product3);
-        Manager mockManager = mock(Manager.class);
-        when(mockManager.getAllProducts()).thenReturn(products);
-        doCallRealMethod().when(mockManager).alertProductExpiration(anyInt());
-
-        // Act
-        mockManager.alertProductExpiration(1);
-
-        // Assert
-        verify(mockManager).sendProductExpirationAlert(product1);
-        verify(mockManager, never()).sendProductExpirationAlert(product2);
-        verify(mockManager, never()).sendProductExpirationAlert(product3);
-    }
-
-    @Test
-    void testAlertProductExpirationWithNonExistingProductId() {
-        // Arrange
-        Manager mockManager = mock(Manager.class);
-        when(mockManager.getProductById(4)).thenReturn(null);
-        doCallRealMethod().when(mockManager).alertProductExpiration(anyInt());
-
-        // Act
-        mockManager.alertProductExpiration(4);
-
-        // Assert
-        verify(mockManager, never()).sendProductExpirationAlert(any(Product.class));
-    }
-
-    @Test
-    void testAlertProductExpiration() {
-        // Arrange
-        ArrayList<Product> products = new ArrayList<>();
-        products.add(product1);
-        products.add(product2);
-        products.add(product3);
-        Manager mockManager = mock(Manager.class);
-        when(mockManager.getAllProducts()).thenReturn(products);
-        doCallRealMethod().when(mockManager).alertProductExpiration();
-
-        // Act
-        mockManager.alertProductExpiration();
-
-        // Assert
-        verify(mockManager).sendProductExpirationAlert(product1);
-        verify(mockManager).sendProductExpirationAlert(product2);
-        verify(mockManager).sendProductExpirationAlert(product3);
-    }
-
-    @Test
-    void testGetProductById() {
-        // Arrange
-        ArrayList<Product> products = new ArrayList<>();
-        products.add(product1);
-        products.add(product2);
-        products.add(product3);
-        Manager mockManager = mock(Manager.class);
-        when(mockManager.getAllProducts()).thenReturn(products);
-        doCallRealMethod().when(mockManager).getProductById(anyInt());
-
-        // Act
-        Product result = mockManager.getProductById(2);
-
-        // Assert
-        assertEquals(product2, result);
-    }
-
-    @Test
-    void testGetAllProducts() {
-        // Arrange
-        ArrayList<Product> products = new ArrayList<>();
-        products.add(product1);
-        products.add(product2);
-        products.add(product3);
-        Manager mockManager = mock(Manager.class);
-        when(mockManager.getAllProducts()).thenReturn(products);
-        doCallRealMethod().when(mockManager).getAllProducts();
-
-        // Act
-        ArrayList<Product> result = mockManager.getAllProducts();
-
-        // Assert
-        assertEquals(products, result);
-    }
-
-    @Test
-    void testSendProductExpirationAlert() {
-        // Arrange
-        Product mockProduct = mock(Product.class);
-        when(mockProduct.getName()).thenReturn("Test Product");
-        Manager manager = new Manager(1, "John Doe");
-    
-        // Act
-        manager.sendProductExpirationAlert(mockProduct);
-    
-        // Assert
-        String expectedAlertMessage = "Product Test Product is almost expired!";
-        assertEquals(expectedAlertMessage, outContent.toString().trim());
-    }
-}
+ import java.time.LocalDate;
+ import java.time.format.DateTimeFormatter;
+ import java.util.ArrayList;
+ 
+ import org.junit.jupiter.api.Test;
+ import static org.junit.jupiter.api.Assertions.*;
+ 
+ public class ProductAlertExpirationTest {
+ 
+     private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+ 
+     @Test
+     void testAlertProductExpiration() {
+         // Arrange
+         Manager manager = new Manager(1, "John Doe");
+         String date = LocalDate.now().plusDays(3).format(formatter);
+         Product product = new Product("Test Product", 1, date, 10.0, 1);
+         boolean[] alertSent = {false};
+         manager.setAlertHandler((p) -> alertSent[0] = true);
+ 
+         // Act
+         manager.alertProductExpiration(1);
+ 
+         // Assert
+         assertTrue(alertSent[0], "Expected an alert to be sent for the almost expired product.");
+     }
+ 
+     @Test
+     void testAlertProductExpirationNoAlert() {
+         // Arrange
+         Manager manager = new Manager(1, "John Doe");
+         String date = LocalDate.now().plusDays(8).format(formatter);
+         Product product = new Product("Test Product", 1, date, 10.0, 1);
+         boolean[] alertSent = {false};
+         manager.setAlertHandler((p) -> alertSent[0] = true);
+ 
+         // Act
+         manager.alertProductExpiration(1);
+ 
+         // Assert
+         assertFalse(alertSent[0], "Expected no alert to be sent for the product that is not almost expired.");
+     }
+ 
+     @Test
+     void testAlertProductExpirationMultipleProducts() {
+         // Arrange
+         Manager manager = new Manager(1, "John Doe");
+         String date1 = LocalDate.now().plusDays(3).format(formatter);
+         String date2 = LocalDate.now().plusDays(5).format(formatter);
+         String date3 = LocalDate.now().plusDays(9).format(formatter);
+         Product product1 = new Product("Test Product 1", 1, date1, 10.0, 1);
+         Product product2 = new Product("Test Product 2", 2, date2, 20.0, 2);
+         Product product3 = new Product("Test Product 3", 3, date3, 30.0, 3);
+         ArrayList<Product> allProducts = new ArrayList<Product>();
+         allProducts.add(product1);
+         allProducts.add(product2);
+         allProducts.add(product3);
+         boolean[] alertSent = {false, false, false};
+         manager.setAlertHandler((p) -> {
+             if (p.equals(product1)) {
+                 alertSent[0] = true;
+             } else if (p.equals(product2)) {
+                 alertSent[1] = true;
+             } else if (p.equals(product3)) {
+                 alertSent[2] = true;
+             }
+         });
+ 
+         // Act
+         manager.alertProductExpiration();
+ 
+         // Assert
+         assertTrue(alertSent[0], "Expected an alert to be sent for the almost expired product 1.");
+         assertTrue(alertSent[1], "Expected an alert to be sent for the almost expired product 2.");
+         assertFalse(alertSent[2], "Expected no alert to be sent for the product that is not almost expired.");
+     }
+ }
+ 
 
         
 
