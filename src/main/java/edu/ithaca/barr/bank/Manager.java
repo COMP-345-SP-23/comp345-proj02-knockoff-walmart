70a44d93efc8768ac4275a4d31e310045f677dde
acfielder <afielder@ithaca.edu>
2023-04-05 11:29:52 -0400

    added setter for product price

--- src/main/java/edu/ithaca/barr/bank/Manager.java
+++ src/main/java/edu/ithaca/barr/bank/Manager.java
@@ -3,6 +3,14 @@ package edu.ithaca.barr.bank;
 import java.util.*;
 import java.time.LocalDate;
 
+
+/**
+ * @classname Manager
+ * @author Arabella Fielder, Liz Richards, Matt Weil
+ * @methods AF: alertLowStock, alertLowStock, orderMoreProduct - LR: addProduct - MW: alertProductExpiration, alertProductExpiration, helper methods
+ * @date 04/03/23
+ */
+
 public class Manager extends Employee{
     
     public Manager(int idIn, String nameIn){
@@ -12,9 +20,9 @@ public class Manager extends Employee{
     /**
      * @param productId id of product to check stock of
      * @return string clarifying if product is low on stock or not as well as how many of the product remaining if low
-     * @throws Exception
+     * @throws NoSuchElementException
      */
-    public String alertLowStock(int productId) throws Exception{
+    public String alertLowStock(int productId) throws NoSuchElementException{
         ArrayList<Product> products = GroceryStore.getProducts();
         Product alertProduct = null;
         for (Product product : products){
@@ -32,7 +40,7 @@ public class Manager extends Employee{
             }
         }
         else{
-            throw new Exception("Product is not in this store");
+            throw new NoSuchElementException("Product is not in this store");
         }
     }
 
@@ -87,8 +95,9 @@ public class Manager extends Employee{
      * @param amountToAdd adding this particular amount to the products inventoryr
      * @post that product's inventory count will be increased
      * @return string either saying you made the order or that that product isnt in the system
+     * @throws NoSuchElementException
      */
-    public void orderMoreProduct(int id, int amountToAdd) throws Exception{
+    public void orderMoreProduct(int id, int amountToAdd) throws NoSuchElementException{
         ArrayList<Product> products = GroceryStore.getProducts();
         Product productToOrder = null;
         for (Product product : products){
@@ -98,7 +107,7 @@ public class Manager extends Employee{
             }
         }
         if (productToOrder == null){
-            throw new Exception("Product does not exist in this store");
+            throw new NoSuchElementException("Product does not exist in this store");
         }
         else{
             productToOrder.increaseInventory(amountToAdd);
--- src/test/java/edu/ithaca/barr/bank/ProductAlertStockTest.java
+++ src/test/java/edu/ithaca/barr/bank/ProductAlertStockTest.java
@@ -4,22 +4,22 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
+import java.util.NoSuchElementException;
 
 /**
  * @classname ProductAlertStockTest
  * @author Arabella Fielder
- * @methods alertLowStockOneTest(), alertLowStockTestAll()
+ * @methods alertLowStockOneTest(), alertLowStockTestAll(), orderMoreProductTest(), incrementDecrementInventoryTest()
  * @date 03/30/23
  */
 
 public class ProductAlertStockTest {
 
     @Test
-    public void alertLowStockOneTest() throws Exception{
+    public void alertLowStockOneTest() throws NoSuchElementException{
         Manager manager = new Manager(1234, "John");
         Product product1 = new Product("Lettuce", 1, "4/5/23", .79, 3245);
-        GroceryStore groceryStore = new GroceryStore();
-        groceryStore.getProducts().add(product1);
+        GroceryStore.getProducts().add(product1);
         assertEquals(10, product1.getInventory()); //ensuring starting inventory is correct
         assertEquals("Product is not running low on stock: 10 remaining", manager.alertLowStock(3245)); //tests for when inventory isnt running low
         product1.decreaseInventory(6);
@@ -27,7 +27,7 @@ public class ProductAlertStockTest {
         assertEquals("Product Inventory Running Low: 4 remaining", manager.alertLowStock(3245));//tests to check alert sent out that inventory is low for greatest amount possible
 
         Product product2 = new Product("Carrots", 1, "4/5/23", 2.49, 3246);
-        groceryStore.getProducts().add(product2);
+        GroceryStore.getProducts().add(product2);
         assertEquals(10, product2.getInventory()); //ensuring starting inventory is correct
         product2.decreaseInventory(10);
         assertEquals(0, product2.getInventory()); //ensures lesser product inventory
@@ -38,14 +38,13 @@ public class ProductAlertStockTest {
 
     @Test
     public void alertLowStockAllTest(){
-        GroceryStore groceryStore = new GroceryStore();
         Manager manager = new Manager(1234, "John");
         Product product1 = new Product("Lettuce", 1, "4/5/23", .79, 3245);
         Product product2 = new Product("Carrots", 1, "4/5/23", 2.49, 3246);
         Product product3 = new Product("Bread", 2, "4/16/23", 3.49, 1243);
-        groceryStore.getProducts().add(product1);
-        groceryStore.getProducts().add(product2);
-        groceryStore.getProducts().add(product3);
+        GroceryStore.getProducts().add(product1);
+        GroceryStore.getProducts().add(product2);
+        GroceryStore.getProducts().add(product3);
 
         assertEquals("No products are running low on stock", manager.alertLowStock());//tests for when in a list of many none are running low on stock
 
@@ -62,13 +61,10 @@ public class ProductAlertStockTest {
 
 
     @Test
-    public void orderMoreProductTest() throws Exception{
-        GroceryStore groceryStore = new GroceryStore();
+    public void orderMoreProductTest() throws NoSuchElementException{
         Manager manager = new Manager(1234, "John");
         Product product1 = new Product("Lettuce", 1, "4/5/23", .79, 3245);
-        groceryStore.getProducts().add(product1);
-        // ArrayList<Product> products = GroceryStore.getProducts();
-        // products.add(product1);
+        GroceryStore.getProducts().add(product1);
         assertEquals(10, product1.getInventory());//checks that starting inventory is 10
         manager.orderMoreProduct(3245, 12);//actual step of ordering more of a product
         assertEquals(22, product1.getInventory());//checks that inventory was increased by particular amount
