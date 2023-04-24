0088d57652c84e8d16f0e5669da79ee9310f0a99
mattweil11 <mattweil11@gmail.com>
2023-04-24 14:29:52 -0400

    Implemented getProduct info

--- src/main/java/edu/ithaca/barr/bank/Employee.java
+++ src/main/java/edu/ithaca/barr/bank/Employee.java
@@ -89,9 +89,16 @@ public class Employee {
     }
 
     //Get Product information and inventory
-    public String getProductInfo(int productId){
-        return "hi";
+    public String getProductInfo(int productId) {
+        for (Product p : GroceryStore.getProducts()) {
+            if (p.getId() == productId) {
+                return String.format("Product: %s - Location: %s - Stock: %d - Price: $%.2f - Expiration date: %s - ID: %d",
+                    p.getName(), p.getLocation(), p.getInventory(), p.getPrice(), p.getDateAsLocalDate(), p.getId());
+            }
+        }
+        return "This product does not exist in this store.";
     }
+    
 
 
 }
--- src/test/java/edu/ithaca/barr/bank/ProductEmployeeTest.java
+++ src/test/java/edu/ithaca/barr/bank/ProductEmployeeTest.java
@@ -79,7 +79,7 @@ class ProductEmployeeTest {
         Employee e = new Employee(0, "Emma" );
         Product p1 = new Product("Banana", "1b", "01/01/2023", 1.69, 1001);
         GroceryStore.getProducts().add(p1);
-        String expected = "Product: Banana - Location: 1b - Stock: 10 - Price: $1.69 - Expiration date: 01/01/2023 - ID: 1001";
+        String expected = "Product: Banana - Location: 1b - Stock: 10 - Price: $1.69 - Expiration date: 2023-01-01 - ID: 1001";
         String actual = e.getProductInfo(1001);
         assertEquals(expected, actual);
     }
@@ -97,20 +97,24 @@ class ProductEmployeeTest {
         GroceryStore.getProducts().add(p3);
         GroceryStore.getProducts().add(p4);
 
-        String expected1 = "Product: Banana - Location: 1b - Stock: 10 - Price: $1.69 - Expiration date: 01/01/2023 - ID: 1001";
+        String expected1 = "Product: Banana - Location: 1b - Stock: 10 - Price: $1.69 - Expiration date: 2023-01-01 - ID: 1001";
         String actual1 = e.getProductInfo(1001);
         assertEquals(expected1, actual1);
 
-        String expected2 = "Product: Orange - Location: 2a - Stock: 10 - Price: $2.50 - Expiration date: 02/02/2023 - ID: 1002";
+        String expected2 = "Product: Orange - Location: 2a - Stock: 10 - Price: $2.50 - Expiration date: 2023-02-02 - ID: 1002";
         String actual2 = e.getProductInfo(1002);
         assertEquals(expected2, actual2);
 
-        String expected3 = "Product: Cucumber - Location: 3a - Stock: 10 - Price: $2.12 - Expiration date: 03/03/2023 - ID: 1003";
+        String expected3 = "Product: Cucumber - Location: 3a - Stock: 10 - Price: $2.12 - Expiration date: 2023-03-03 - ID: 1003";
         String actual3 = e.getProductInfo(1003);
         assertEquals(expected3, actual3);
 
-        String expected4 = "Product: Apple - Location: 4b - Stock: 10 - Price: $4.01 - Expiration date: 04/04/2023 - ID: 1004";
+        String expected4 = "Product: Apple - Location: 4b - Stock: 10 - Price: $4.01 - Expiration date: 2023-04-04 - ID: 1004";
         String actual4 = e.getProductInfo(1004);
         assertEquals(expected4, actual4);
+
+        String expected5 = "This product does not exist in this store.";
+        String actual5 = e.getProductInfo(1005);
+        assertEquals(expected5, actual5);
     }
 }
